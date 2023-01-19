baa3b9541f12c925f2e79efcc700edf84d0fa8d9
VIDUSHI SINGHAL <55014309+Vidushi1610@users.noreply.github.com>
2023-01-19 13:39:32 +0530

    Update VideoCapture.py

--- VideoCapture.py
+++ VideoCapture.py
@@ -1,8 +1,9 @@
 import cv2
-capture = cv2.VideoCapture(1)
-
+capture = cv2.VideoCapture(0) 
 while(True):
-  ret,frame= capture.read()
-  cv2.imshow('frame', frame)
-  if cv2.waitkey(1) == ord('v'):
-    break
+
+    ret, frame = capture.read()
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1)  == ord('v') :
+        break
+
