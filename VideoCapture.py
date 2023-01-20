486795c0099b40d85d5bd0aee8e227009d66efbb
VIDUSHI SINGHAL <55014309+Vidushi1610@users.noreply.github.com>
2023-01-20 15:31:07 +0530

    Update VideoCapture.py

--- VideoCapture.py
+++ VideoCapture.py
@@ -1,8 +1,11 @@
 import cv2
+import imutils
+
 capture = cv2.VideoCapture(0) 
 while(True):
 
     ret, frame = capture.read()
+    frame = imutils.resize(frame, width=600)
     cv2.imshow('frame', frame)
     if cv2.waitKey(1)  == ord('v'):
         break
